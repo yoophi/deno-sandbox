a4fcb06891e9d57fb07641a817176a3790ccb4cf
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-16 23:55:24 +0900

    ✨ NEW: sample middlewares

--- planet-api/mod.ts
+++ planet-api/mod.ts
@@ -3,7 +3,20 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.request.headers.get("x-response-time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("x-response-time", `${delta}ms`);
+});
+
+app.use(async (ctx, next) => {
   ctx.response.body = `
  _   _    _    ____    _    
 | \\ | |  / \\  / ___|  / \\   
@@ -11,6 +24,7 @@ app.use((ctx) => {
 | |\\  |/ ___ \\ ___) / ___ \\ 
 |_| \\_/_/   \\_\\____/_/   \\_\\
 `;
+  await next();
 });
 
 if (import.meta.main) {
